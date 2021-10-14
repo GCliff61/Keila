a38fb5284a50f1889ae5635678d4ef8a2a9ece5c
GCliff61 <isto.kallio@gmail.com>
2021-10-14 14:31:05 +0300

    Aloitus taas

---
+++ testi99.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+//phpinfo();
+
+$php = "PHP";
+$java = "Java";
+$perl = "Perl";
+$jscript = "Javascript";
+$ots = "ohjelmointikielet";
+
+echo "<h2>".$ots."</h2>";
+
+echo "<ul>";
+  echo "<li>".$php."</li>";
+  echo "<li>".$java."</li>";
+  echo "<li>".$perl."</li>";
+  echo "<li>".$jscript."</li>";
+echo "</ul>";
+
+?>
+
+
+</body>
+</html>
