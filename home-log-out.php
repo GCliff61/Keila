06eda315bc363e84187f6d412ec1ed5af0ed54eb
GCliff61 <isto.kallio@gmail.com>
2021-10-18 17:42:03 +0300

    Näytöt paikalliseen repositoryyn

---
+++ home-log-out.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css">
+<style>
+/* muotoiluasiat siirretty css-tiedostoon */
+</style>
+</head>
+
+<body>
+<nav class="navbar">
+     <!-- LOGO 
+     <div class="logo">
+         <img src="keilalogo_vaaka.jpg" alt="kuva puuttuu" width="200" height="50">
+     </div> 
+     -->
+     <!-- NAVIGATION MENU -->
+     <ul class="nav-links">
+       <!-- USING CHECKBOX HACK -->
+       <input type="checkbox" id="checkbox_toggle" />
+       <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+       <!-- NAVIGATION MENUS -->
+       <div class="menu">
+         
+         <li><a href="home-log-out.php"><i class="fa fa-fw fa-home"></i>Koti</a></li>
+         <li><a href="login.php">Kirjaudu sisään/rekisteröidy</a></li>
+         <li class="services">
+           <a href="/">Services</a>
+           <!-- DROPDOWN MENU -->
+           <ul class="dropdown">
+             <li><a href="/">Dropdown 1 </a></li>
+             <li><a href="/">Dropdown 2</a></li>
+             <li><a href="/">Dropdown 2</a></li>
+             <li><a href="/">Dropdown 3</a></li>
+             <li><a href="/">Dropdown 4</a></li>
+           </ul>
+         </li>
+         <li><a href="palaute.php">Anna palautetta</a></li>
+         <li><a href="logout.php">Kirjaudu ulos</a></li>
+       </div>
+     </ul>
+   </nav>
+
+    
+<h1 class="kotitxt">Tervetuloa Tuuri-keilaajien kotisivulle</br>
+</br>
+<h2 class="kotitxt2">Paikka, josta löydät helposti keilahallit ja pro-shopit</br>
+<h2 class="kotitxt3">vastuuhenkilöineen ja yhteystietoineen!</br>
+
+<div class="footer">
+  <p>Footerien footer &copy;</p>
+</div>
+
+</body>
+</html>
