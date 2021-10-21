cd57674ecbb980c95ce841b9796e803507983a85
GCliff61 <isto.kallio@gmail.com>
2021-10-22 00:06:19 +0300

    login parantelua

--- home-log-out.php
+++ home-log-out.php
@@ -5,6 +5,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style.css">
+<!-- jQuery + Bootstrap JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
@@ -45,12 +49,134 @@
 <h3 class="kotitxt2">Paikka, josta löydät helposti keilahallit ja pro-shopit</br>
 <h3 class="kotitxt3">vastuuhenkilöineen ja yhteystietoineen!</br>
 
-<?php
+
+
+
+
+    <?php
+   
     include ("dbconnect_keila.php");
+   
+
+    global $wrongPwdErr, $accountNotExistErr, $emailPwdErr, $verificationRequiredErr, $email_empty_err, $pass_empty_err;
+
+    if(isset($_POST['login'])) {
+        $email_signin        = $_POST['email_signin'];
+        $password_signin     = $_POST['password_signin'];
+
+        // clean data 
+        $user_email = filter_var($email_signin, FILTER_SANITIZE_EMAIL);
+        $pswd = mysqli_real_escape_string($yhteys, $password_signin);
+
+        // Query if email exists in db
+        $sql = "SELECT * From users WHERE email = '{$email_signin}' ";
+        $query = mysqli_query($yhteys, $sql);
+        $rowCount = mysqli_num_rows($query);
+
+        // If query fails, show the reason 
+        if(!$query){
+           die("SQL query failed: " . mysqli_error($yhteys));
+        }
+
+        if(!empty($email_signin) && !empty($password_signin)){
+            if(!preg_match("/^(?=.*\d)(?=.*[@#\-_$%^&+=§!\?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]{6,20}$/", $pswd)) {
+                $wrongPwdErr = '<div class="alert alert-danger">
+                        Password should be between 6 to 20 charcters long, contains atleast one special chacter, lowercase, uppercase and a digit.
+                    </div>';
+            }
+            // Check if email exist
+            if($rowCount <= 0) {
+                $accountNotExistErr = '<div class="alert alert-danger">
+                        User account does not exist.
+                    </div>';
+            } else {
+                // Fetch user data and store in php session
+                while($row = mysqli_fetch_array($query)) {
+                    $id            = $row['id'];
+                    $firstname     = $row['firstname'];
+                    $lastname      = $row['lastname'];
+                    $email         = $row['email'];
+                    $mobilenumber   = $row['mobilenumber'];
+                    $pass_word     = $row['password'];
+                    $token         = $row['token'];
+                    $is_active     = $row['is_active'];
+                }
+
+                // Verify password
+                $password = password_verify($password_signin, $pass_word);
+
+                // Allow only verified user
+                if($is_active == '1') {
+                    if($email_signin == $email && $password_signin == $password) {
+                       header("Location: home-log-out.php");
+                       
+                       $_SESSION['id'] = $id;
+                       $_SESSION['firstname'] = $firstname;
+                       $_SESSION['lastname'] = $lastname;
+                       $_SESSION['email'] = $email;
+                       $_SESSION['mobilenumber'] = $mobilenumber;
+                       $_SESSION['token'] = $token;
+
+                    } else {
+                        $emailPwdErr = '<div class="alert alert-danger">
+                                Either email or password is incorrect.
+                            </div>';
+                    }
+                } else {
+                    $verificationRequiredErr = '<div class="alert alert-danger">
+                            Account verification is required for login.
+                        </div>';
+                }
+
+            }
+
+        } else {
+            if(empty($email_signin)){
+                $email_empty_err = "<div class='alert alert-danger email_alert'>
+                            Email not provided.
+                    </div>";
+            }
+            
+            if(empty($password_signin)){
+                $pass_empty_err = "<div class='alert alert-danger email_alert'>
+                            Password not provided.
+                        </div>";
+            }            
+        }
+
+    }
+
 ?>
 
- 
+    <!-- Login form -->
+    <div class="App">
+        <div class="vertical-center">
+            <div class="inner-block">
+                <form action="" method="post">
+                    <h3>Login</h3>
+                    <?php echo $accountNotExistErr; ?>
+                    <?php echo $emailPwdErr; ?>
+                    <?php echo $verificationRequiredErr; ?>
+                    <?php echo $email_empty_err; ?>
+                    <?php echo $pass_empty_err; ?>
 
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="email" class="form-control" name="email_signin" id="email_signin" />
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" name="password_signin" id="password_signin" />
+                    </div>
+
+                    <button type="submit" name="login" id="sign_in"
+                        class="btn btn-outline-primary btn-lg btn-block">Sign
+                        in</button>
+                </form>
+            </div>
+        </div>
+    </div>
 
 <div class="footer">
   <p>Footerien footer &copy;</p>
